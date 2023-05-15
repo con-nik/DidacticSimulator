0fc97976c166c01cf0fa2e340140bd04e8a96290
Con Nik <conn.nik1@gmail.com>
2023-05-15 20:47:03 +0300

    Update Assembler

--- Utilities/Parser.cs
+++ Utilities/Parser.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -12,19 +13,32 @@ namespace DidacticSimulator.Utilities
 {
     public class Parser
     {
-        private string labelMatch = @"[A-Za-z]+:";
+        private string labelMatch = @"[A-Za-z0-9]+:";
         private string onlyUpperCaseMatch = @"\b[A-Z]+\b(?![^ ]*[^A-Za-z\s])";
+        private string operandsMatch = @"(R\d+)|(\d+\(R\d+\))|(\(R\d+\))|(\d+)";
+
+        private string adMatch = @"(R\d+)";
+        private string amMatch = @"(\d+)";
+        private string aiMatch = @"(\(R\d+\))";
+        private string axMatch = @"(\d+\(R\d+\))";
 
         private Dictionary<int, string> _instructions;
         private Dictionary<string, int> _labels;
+        private Dictionary<int, List<string>> _operands;
+        private Dictionary<int, string> _lines;
 
 
-        public Parser(string pathFile)
+        public Parser()
         {
-            List<string> fileLines = new List<string>();
-
             _instructions = new Dictionary<int, string>();
             _labels = new Dictionary<string, int>();
+            _operands = new Dictionary<int, List<string>>();
+            _lines = new Dictionary<int, string>();
+        }
+
+        public void ParseFile(string pathFile)
+        {
+            List<string> fileLines = new List<string>();
 
             using (StreamReader sr = new StreamReader(pathFile))
             {
@@ -38,46 +52,50 @@ namespace DidacticSimulator.Utilities
 
             for (int index = 0; index < fileLines.Count; index++)
             {
+                _lines.Add(index, fileLines[index]);
+
                 Match match = Regex.Match(fileLines[index], onlyUpperCaseMatch);
                 if (match.Success)
                     _instructions.Add(index, match.Value);
 
                 Match matchLabels = Regex.Match(fileLines[index], labelMatch);
                 if (matchLabels.Success)
-                    _labels.Add(matchLabels.Value, index);
-            }
+                    _labels.Add(matchLabels.Value.Substring(0, matchLabels.Value.Length - 1), index);
 
+                MatchCollection matchOperands = Regex.Matches(fileLines[index], operandsMatch);
+                List<string> operands = new List<string>();
+                foreach (Match matchOperand in matchOperands)
+                {
+                    operands.Add(getAddressingMode(matchOperand.Value));
+                    operands.Add(matchOperand.Value);
+                }
+                _operands.Add(index, operands);
+            }
+        }
+
+        private string getAddressingMode(string operand) {
+            Match matchAx = Regex.Match(operand, axMatch);
+            if (matchAx.Success)
+                return "11";
+
+            Match matchAi = Regex.Match(operand, aiMatch);
+            if (matchAi.Success)
+                return "10";
+
+            Match matchAd = Regex.Match(operand, adMatch);
+            if (matchAd.Success)
+                return "01";
+
+            Match matchAm = Regex.Match(operand, amMatch);
+            if (matchAm.Success)
+                return "00";
+
+            return "";
         }
 
         public Dictionary<int, string> Instructions => _instructions;
         public Dictionary<string, int> Labels => _labels;
-
-        /* public void parseInputFile(string pathFile)
-         {
-             for (int index = 0; index < fileLines.Count; index++)
-             {
-                 Match match = Regex.Match(fileLines[index], onlyUpperCaseMatch);
-                 if (match.Success)
-                     instructions.Add(index, match.Value);
-
-                 Match matchLabels = Regex.Match(fileLines[index], labelMatch);
-                 if (matchLabels.Success)
-                     labels.Add(matchLabels.Value, index);
-             }
-
-
-             using (StreamWriter writer = new StreamWriter(@"..\..\Shared\Files\OutputFile.txt"))
-             {
-                 foreach (var kvp in instructions)
-                 {
-                     writer.Write($"{kvp.Key} # {kvp.Value} \n");
-                 }
-
-                 foreach (var kvp in labels)
-                 {
-                     writer.Write($"{kvp.Key} # {kvp.Value} \n");
-                 }
-             }
-         }*/
+        public Dictionary<int, List<string>> Operands => _operands;
+        public Dictionary<int, string> Lines => _lines;
     }
 }
